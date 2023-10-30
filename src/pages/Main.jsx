4afe2f6aa361532c54c429f859c4813c29d577fb
DevTinaTi <tinadesk8@gmail.com>
2023-10-30 14:21:59 +0100

    add box-sizing for all elemenents

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -176,7 +176,7 @@ function Main() {
         <nav className="over-lay-nav">
           <AiOutlineClose
             className="close"
-            style={{  height: "25%" }}
+            style={{  height: "15%" }}
           onClick={open}/>
           <ul>
             <li>home</li>
